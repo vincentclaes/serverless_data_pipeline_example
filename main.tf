4fc15e289a68e0d485bc610432d85ce776500c44
Vincent Claes <vincent@Vincents-MacBook-Pro.local>
2019-07-06 16:33:49 +0200

    add lambda but you need to zip it yourself :(

--- main.tf
+++ main.tf
@@ -4,20 +4,38 @@ provider "aws" {
   region = "eu-central-1"
 }
 resource "aws_s3_bucket" "example" {
-  bucket = "serverless-data-pipeline-${var.stage}-example"
+  bucket = "serverless-data-pipeline-${var.stage}-glue-script"
   acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-    Stage = "${var.stage}"
-  }
 }
-resource "aws_glue_job" "example" {
-  name     = "serverless-data-pipelinep-${var.stage}"
+resource "aws_s3_bucket_object" "object" {
+  bucket = "serverless-data-pipeline-${var.stage}-glue-script"
+  key    = "glue_unzip.py"
+  source = "glue/glue_unzip.py"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = "${filemd5("glue/glue_unzip.py")}"
+}
+resource "aws_glue_job" "glue_unzip" {
+  name     = "serverless-data-pipeline-${var.stage}"
   role_arn = "arn:aws:iam::077590795309:role/glue-admin"
-
+  max_capacity = 1
   command {
-    script_location = "s3://${aws_s3_bucket.example.bucket}/example.py"
+    name="pythonshell"
+    script_location = "s3://${aws_s3_bucket.example.bucket}/glue_unzip.py"
   }
+  execution_property {
+    max_concurrent_runs = 2
+  }
+
 }
+
+# resource "aws_lambda_function" "lambda_function" {
+#   role             = "arn:aws:iam::077590795309:role/serverless-data-pipeline-lambda"
+#   handler          = "${var.handler}"
+#   runtime          = "python3.6"
+#   filename         = "lambda.zip"
+#   function_name    = "${var.function_name}"
+#   source_code_hash = "${base64sha256(file("lambda.zip"))}"
+# }
