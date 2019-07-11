2fbc781f1aaf64445ac7fe09345ec4266ee6e534
vincent <vclaes1986@gmail.com>
2019-07-11 21:41:36 +0200

    add glue crawler job

--- lambda/load.py
+++ lambda/load.py
@@ -1,32 +1,15 @@
 import os
 import boto3
 
-database = os.getenv('ATHENA_DB')
-table = os.getenv('ATHENA_TABLE')
-bucket = os.getenv('DEST_BUCKET')
-s3_key = os.getenv('DEST_KEY')
-athena_client = boto3.client('athena')
-config = {'OutputLocation': 's3://{}/athena_output'.format(bucket)}
-result_create_db = athena_client.start_query_execution(
-    QueryString='CREATE DATABASE IF NOT EXISTS {};'.format(database),
-    ResultConfiguration=config
-)
-print('create db {}'.format(result_create_db))
-context = {'Database': '{}'.format(bucket)}
-result_create_table = athena_client.start_query_execution(QueryString='''
-CREATE EXTERNAL TABLE IF NOT EXISTS {}.{} (
-      `id` string,
-      `from` string,
-      `to` string,
-      `cc` string,
-      `subject` string,
-      `date` string,
-      `body` string
-      )
-      ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-      WITH SERDEPROPERTIES ('serialization.format' = '1')
-      LOCATION 's3://{}/{}'
-      TBLPROPERTIES ('has_encrypted_data'='false');
-'''.format(database, table, bucket, s3_key), QueryExecutionContext=context, ResultConfiguration=config)
 
-print('create table {}'.format(result_create_table))
+def handler(event, context):
+    job_name = os.getenv('GLUE_CRAWLER')
+    glue_client = boto3.client('glue')
+    crawler_metadata = glue_client.get_crawler(Name=job_name)
+    crawler_state = crawler_metadata.get('Crawler').get('State')
+    if crawler_state == 'READY':
+        print('crawler is ready, so we are starting it ...')
+        result = glue_client.start_crawler(Name=job_name)
+        print('crawler result {}'.format(result))
+    print('crawler is in state {}. only start crawler when in state READY'.format(crawler_state))
+    return crawler_state
