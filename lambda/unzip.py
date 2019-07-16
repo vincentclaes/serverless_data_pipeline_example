a16cd79db4b64b7abc48e600ef61275f180c4dd4
vincent <vclaes1986@gmail.com>
2019-07-16 11:10:07 +0200

    add variables

--- lambda/unzip.py
+++ lambda/unzip.py
@@ -6,11 +6,13 @@ def handler(event, context):
     """
     trigger a glue job that will unzip zip files stored on s3.
     """
-    # get the source key and bucket from the event that triggered this lambda function.
+    # get the source key and bucket from
+    # the event that triggered this lambda function.
     source_key = event['Records'][0]['s3']['object']['key']
     source_bucket = event['Records'][0]['s3']['bucket']['name']
 
-    # get the destination key and bucket from the environment variables
+    # get the destination key and bucket from
+    # the environment variables
     glue_job = os.getenv('GLUE_JOB')
     destination_bucket = os.getenv('DEST_BUCKET')
     destination_key = os.getenv('DEST_KEY')
@@ -23,5 +25,7 @@ def handler(event, context):
         '--destination_key': destination_key,
     }
     print('args for glue job: {}'.format(args))
-    result = boto3.client('glue').start_job_run(JobName=glue_job, Arguments=args)
+
+    result = boto3.client('glue')\
+        .start_job_run(JobName=glue_job, Arguments=args)
     return result
